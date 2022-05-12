d624fd4d582565f5007af729307a1e5adb19741a
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-12 14:15:01 -0300

    update
    update

---
+++ Aula02_DominandoAspNetMVCCore/Aula04_Views/A.4_DemoMvcViews/A.4_DemoMvcViews/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using A._4_DemoMvcViews.Models;
+using System.Diagnostics;
+namespace A._4_DemoMvcViews.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItensCarrinho = 3;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
