9a7d15ae6a7ee1cb198003c56ebc6222eb6f753c
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-13 13:41:03 -0300

    criação do projeto prático  de Modelo MVC aula 05

---
+++ Aula02_DominandoAspNetMVCCore/Aula05_MinhaPrimeiraAppMVCCore/AppModelo/src/DevIO.UI.Site/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.UI.Site
+{
+    public class Startup
+    {
+        //This method gest called by runTime . Use this method to add services to the container.
+        //For more information on how to configure your application,visit http://go.microsoft.com/fwlink/?/LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        //This method gets called by the runTime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
