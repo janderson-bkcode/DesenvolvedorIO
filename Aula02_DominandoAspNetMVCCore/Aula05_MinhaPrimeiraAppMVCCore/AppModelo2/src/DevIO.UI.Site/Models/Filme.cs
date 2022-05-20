13818efcf35834f0c7f8986be6d66def9896319f
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-20 15:46:34 -0300

    update

---
+++ Aula02_DominandoAspNetMVCCore/Aula05_MinhaPrimeiraAppMVCCore/AppModelo2/src/DevIO.UI.Site/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+
+namespace DevIO.UI.Site.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+
+        [Display(Name = "Gênero")]
+        public string Genero { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Data de Lancamento")]
+        public DateTime DataLancamento { get; set; }
+
+        public Filme()
+        {
+
+        }
+    }
+}
