133ddecf0c0897eac2beaba692f85e9b776280a1
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-06-14 18:21:08 -0300

    update

---
+++ Aula03_RestAspNetCoreWebAPI/Aula03_NovoProjeto/MinhaPrimeiraAPI/Model/APIDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace MinhaPrimeiraAPI.Model
+{
+    public class APIDbContext :DbContext
+
+
+    {
+
+        
+        public APIDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        //Em vez de fazer na Program/Startup , esse override configura tambem para fazer a Migration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("MeuDbContext");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
--- Aula03_RestAspNetCoreWebAPI/Aula03_NovoProjeto/MinhaPrimeiraAPI/Program.cs
+++ Aula03_RestAspNetCoreWebAPI/Aula03_NovoProjeto/MinhaPrimeiraAPI/Program.cs
@@ -1,3 +1,13 @@
+using MinhaPrimeiraAPI.Model;
+using System.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc();
+builder.Services.AddMvcCore();
+
+//Configuração do DBContext e SQl 
+builder.Services.AddDbContext<APIDbContext>(optionsAction:options =>
+         options.UseSqlServer(builder.Configuration.GetConnectionString(name: "MeuDbContext")));
 
 var app = builder.Build();
 
