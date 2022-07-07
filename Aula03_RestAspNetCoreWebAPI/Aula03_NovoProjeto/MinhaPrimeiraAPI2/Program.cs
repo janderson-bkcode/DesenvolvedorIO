8f7175735666ec94c3b36a24c78c9d8998debfb0
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-07 12:05:59 -0300

    update

--- Aula03_RestAspNetCoreWebAPI/Aula03_NovoProjeto/MinhaPrimeiraAPI2/Program.cs
+++ Aula03_RestAspNetCoreWebAPI/Aula03_NovoProjeto/MinhaPrimeiraAPI2/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.OpenApi.Models;
 using MinhaPrimeiraAPI2.Data;
 using MinhaPrimeiraAPI2.Config;
 using MinhaPrimeiraAPI2.Models;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,39 +15,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 //Classe apiConfig.cs
 builder.Services.WebApiConfig();
-//Classe DependencyInjectionConfig.cs
-
 //Classe SwaggerConfig.cs
 builder.Services.AddSwaggerConfig();
-
+//Classe DependencyInjectionConfig.cs
 builder.Services.ResolveDependencies();
-
-
 builder.Services.AddMvc();
 builder.Services.AddMvcCore();
-
-builder.Services.AddApiVersioning(options =>
-{
-    //Assume a versão default quando não for especificado
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    //Definindo maior e menor versão
-     options.DefaultApiVersion = new ApiVersion(majorVersion: 2, minorVersion: 1);
-    //Quando consumir ele passar no header do response se estã obsoleta ou versão atual
-    options.ReportApiVersions = true;
-});
-builder.Services.AddVersionedApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v',VVV";
-    options.SubstituteApiVersionInUrl = true;
-});
 
 //Configuração do DBContext e SQl 
 builder.Services.AddDbContext<APIDbContext>(optionsAction: options =>
          options.UseSqlServer(builder.Configuration.GetConnectionString(name: "DefaultConnection")));
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -56,10 +36,12 @@ if (app.Environment.IsDevelopment())
     {
         c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "My Api v1");
 
-    });
+    }); 
+
+
 }
-
-
+//Dentro do configure
+//app.app.UseSwaggerConfig(provider);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
