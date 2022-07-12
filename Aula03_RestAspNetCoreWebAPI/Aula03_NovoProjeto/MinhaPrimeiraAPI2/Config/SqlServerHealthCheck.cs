9659bcd3127f8470411888ce08b5f0689b1e5d3c
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-12 10:05:01 -0300

    update

---
+++ Aula03_RestAspNetCoreWebAPI/Aula03_NovoProjeto/MinhaPrimeiraAPI2/Config/SqlServerHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MinhaPrimeiraAPI2.Config
+{
+    public class SqlServerHealthCheck : IHealthCheck
+    {
+        private readonly string _connection;
+
+        public SqlServerHealthCheck(string connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using(var connection = new SqlConnection(_connection)) { 
+
+                    await connection.OpenAsync(cancellationToken);
+
+                    var command  = connection.CreateCommand();
+                    command.CommandText = "Select count(id) from Produtos";
+
+                    return Convert.ToInt32(await command.ExecuteScalarAsync(cancellationToken)) > 0 ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy();
+                }
+            }
+            catch(Exception ex)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
