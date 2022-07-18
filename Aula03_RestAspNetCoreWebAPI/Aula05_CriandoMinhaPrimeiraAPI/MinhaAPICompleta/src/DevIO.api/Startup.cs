b69f1cc1a53763a3eb0cf9a1596014c186bc28bd
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-18 12:26:41 -0300

    update

--- Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Startup.cs
+++ Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Startup.cs
@@ -1,9 +1,11 @@
 using DevIO.api.Config;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,10 @@ namespace DevIO.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MeuDbContext>(options => {
+
+                options.UseSqlServer(Configuration.GetConnectionString(name: "DefaultConnection"));
+            });
             services.AddControllers();
             services.AddSwaggerConfig();
             services.ResolveDependencies();
