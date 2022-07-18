a90893df78a7137175a8f2171442e0045a0f8885
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-18 10:37:46 -0300

    update

---
+++ Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Data/APIDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DevIO.api.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DevIO.api.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<FornecedorViewModel> Fornecedores { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    var configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json")
+        //        .Build();
+
+        //    var connectionString = configuration.GetConnectionString("DefaultConnection");
+        //    optionsBuilder.UseSqlServer(connectionString);
+        //}
+    }
+}
