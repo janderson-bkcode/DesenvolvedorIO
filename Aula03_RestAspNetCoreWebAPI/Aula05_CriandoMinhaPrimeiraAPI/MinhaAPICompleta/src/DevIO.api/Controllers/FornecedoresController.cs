68ca506475cdedeaab2bff153dc1357d7259602d
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-18 11:00:04 -0300

    update

---
+++ Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Controllers/FornecedoresController.cs
@@ -0,0 +1,17 @@
+﻿using DevIO.api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //Validação de notificacoes de erro
+
+        //Validação de modelState
+
+        //Validação da operação de negocios
+    }
+}
