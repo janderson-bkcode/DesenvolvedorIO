e24edce6b78f4b30296c305e9c27d2b2bd9b5cc9
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-20 11:51:50 -0300

    update

--- Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.Business/Models/Validations/FornecedorValidation.cs
+++ Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.Business/Models/Validations/FornecedorValidation.cs
@@ -14,18 +14,20 @@ namespace DevIO.Business.Models.Validations
 
             When(f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, () =>
             {
+                //Comparação de Tamanho do Documento e compara com o método de Tamanho CPF da classe CpfValidacao
                 RuleFor(f => f.Documento.Length).Equal(CpfValidacao.TamanhoCpf)
                     .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
+                //Verifica se o campo atende a validação conforme método Validar da classe CpfValidacao
                 RuleFor(f=> CpfValidacao.Validar(f.Documento)).Equal(true)
                     .WithMessage("O documento fornecido é inválido.");
             });
 
             When(f => f.TipoFornecedor == TipoFornecedor.PessoaJuridica, () =>
             {
-                //Comparação de Tamanho do Documento
+                //Comparação de Tamanho do Documento e compara com o método de Tamanho CPF da classe CnpjValidacao
                 RuleFor(f => f.Documento.Length).Equal(CnpjValidacao.TamanhoCnpj)
                     .WithMessage("O campo Documento precisa ter {ComparisonValue} caracteres e foi fornecido {PropertyValue}.");
-                //Comparação de Tamanho do Documento
+                //Verifica se o campo atende a validação conforme método Validar da classe CnpjValidacao
                 RuleFor(f => CnpjValidacao.Validar(f.Documento)).Equal(true)
                     .WithMessage("O documento fornecido é inválido.");
             });
