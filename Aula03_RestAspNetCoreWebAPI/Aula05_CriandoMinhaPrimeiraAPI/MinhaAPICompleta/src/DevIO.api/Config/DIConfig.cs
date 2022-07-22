5a220a8560ceac2dba15fc5c086be5937fa06da1
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-22 11:15:05 -0300

    udpate

--- Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Config/DIConfig.cs
+++ Aula03_RestAspNetCoreWebAPI/Aula05_CriandoMinhaPrimeiraAPI/MinhaAPICompleta/src/DevIO.api/Config/DIConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
 using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
@@ -12,12 +13,22 @@ namespace DevIO.api.Config
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
-
+            //Repositorios
             services.AddScoped<MeuDbContext>();
-            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();           
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+           
+
+            //Serviços
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IProdutoService, ProdutoService>();
             services.AddScoped<IFornecedorService, FornecedorService>();
-           // services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
             return services;
+
+            //Extensões
+            // services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
         }
     }
 }
