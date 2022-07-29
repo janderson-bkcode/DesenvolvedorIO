8b8b6ad2c20f0263cde155027bd1b1b5b0d8b0df
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-29 10:28:50 -0300

    update

--- Aula04_AspNetCoreEnterpriseApplications/03/NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ Aula04_AspNetCoreEnterpriseApplications/03/NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,11 +9,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace NSE.Identidade.API
@@ -39,7 +42,30 @@ namespace NSE.Identidade.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            
+
+            services.AddAuthentication(options =>
+            {
+                //A forma de autenticação e credenciamento será do tipo  padrão jwt
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("x")),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = "x",
+                    ValidIssuer  ="x"
+
+                };
+            });
+
+
+
             services.AddControllers();
 
             //Configuração documentação Swagger
