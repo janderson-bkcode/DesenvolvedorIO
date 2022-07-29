138e3b8806fe0a8dc3bc88742a1088d2b66b782f
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-07-29 10:10:12 -0300

    update

--- Aula04_AspNetCoreEnterpriseApplications/03/NerdStoreEnterprise/src/services/NSE.Identidade.API/Models/UserViewModels.cs
+++ Aula04_AspNetCoreEnterpriseApplications/03/NerdStoreEnterprise/src/services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -2,33 +2,30 @@
 
 namespace NSE.Identidade.API.Models
 {
-    public class UserViewModels
-    {
-    }
     public class UsuarioRegistro
     {
-        [Required(ErrorMessage ="O campo {0} é obrigatório")]
-        [EmailAddress(ErrorMessage ="O Campo {0} está em formato Invalido")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O Campo {0} está em formato Invalido")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage =("O campo {0} é obrigatório"))]
-        [StringLength(100,ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres",MinimumLength = 6)]
-        public string  Senha { get; set; }
+        [Required(ErrorMessage = ("O campo {0} é obrigatório"))]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Senha { get; set; }
 
-        [Compare("Senha",ErrorMessage ="As senhas não conferem.")]
-        public string  SenhaConfirmacao { get; set; }
+        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
+        public string SenhaConfirmacao { get; set; }
 
     }
 
     public class UsuarioLogin
     {
-        [Required(ErrorMessage =("O campo {0} é obrigatório"))]
-        [EmailAddress(ErrorMessage ="O campo {0} está em formato invalido")]
+        [Required(ErrorMessage = ("O campo {0} é obrigatório"))]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato invalido")]
 
         public string Email { get; set; }
 
-        [Required(ErrorMessage ="O campo {0} é obrigatório")]
-        [StringLength(100,ErrorMessage ="O campo {0} precisa estar entre {2} e {1} caracteres",MinimumLength =6)]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa estar entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Senha { get; set; }
 
     }
--- Aula04_AspNetCoreEnterpriseApplications/03/NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ Aula04_AspNetCoreEnterpriseApplications/03/NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
 using System;
 using System.Collections.Generic;
@@ -40,6 +41,17 @@ namespace NSE.Identidade.API
 
             
             services.AddControllers();
+
+            services.AddSwaggerGen( c => 
+            {
+                c.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title ="NerdStore Entreprise Identity API",
+                    Description="Esta API faz parte do curso ASP.NET Core Enterprise Applications",
+                    Contact = new OpenApiContact() { Name="Eduardo Pires",Email="contato@desenvolvedor.io"},
+                    License = new OpenApiLicense() { Name ="MIT",Url= new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +75,9 @@ namespace NSE.Identidade.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
     }
 }
