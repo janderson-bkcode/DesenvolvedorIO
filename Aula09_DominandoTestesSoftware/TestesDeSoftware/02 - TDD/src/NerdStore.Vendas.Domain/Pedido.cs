7d8939e7172cb9e52725a87742250c5c20cfae04
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-08-02 16:59:54 -0300

    udpate teste aula 09.04.05

--- Aula09_DominandoTestesSoftware/TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
+++ Aula09_DominandoTestesSoftware/TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace NerdStore.Vendas.Domain.Tests
 {
     public class Pedido
 
-
     {
-        public decimal ValorTotal { get; set; }
+        public Pedido()
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+        public decimal ValorTotal { get; private set; }
+        // public Collection<PedidoItem> pedidoItems { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItens;
+
+        public IReadOnlyCollection<PedidoItem> PedidoItem => _pedidoItens;
         public void AdicionarItem(PedidoItem pedidoItem)
         {
-            
+            _pedidoItens.Add(pedidoItem);
+            ValorTotal = _pedidoItens.Sum(i => i.Quantidade * i.ValorUnitario);
         }
     }
     public class PedidoItem
@@ -24,13 +35,13 @@ namespace NerdStore.Vendas.Domain.Tests
             ValorUnitario = valorUnitario;
         }
 
-        public Guid  ProdutoId { get; set; }
+        public Guid  ProdutoId { get; private set; }
 
-        public string ProdutoNome { get; set; }
+        public string ProdutoNome { get; private set; }
 
-        public int Quantidade { get; set; }
+        public int Quantidade { get; private set; }
 
-        public decimal ValorUnitario { get; set; }
+        public decimal ValorUnitario { get; private set; }
 
         
 
