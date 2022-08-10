3936eb9e897d8ec85a6c753a1956a9d6200c2ad9
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-08-10 13:02:44 -0300

    update

--- Aula09_DominandoTestesSoftware/03/TestesDeSoftware2/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
+++ Aula09_DominandoTestesSoftware/03/TestesDeSoftware2/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -18,12 +18,27 @@ namespace Demo.Tests
 
             //Act
 
-            var resultado = calculadora.Somar(10, 2);
+            var resultado = calculadora.Somar(9, 2);
 
             //Assert
            // Assert.True(resultado == 4);
             Assert.Equal(4,resultado);
 
+        }
+
+        [Theory]
+        [InlineData(1,1,2)]
+
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1,double v2,double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            //Assert
+            Assert.Equal(total,resultado);
         }
     }
 }
