13e4846de6d4aa7bea20cf0c8f03ea335958b408
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-08-10 11:53:50 -0300

    update

---
+++ Aula09_DominandoTestesSoftware/03/TestesDeSoftware2/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        /// Todos os métodos do teste são publicos 
+
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            //Arrange
+
+            var calculadora = new Calculadora();
+
+            //Act
+
+            var resultado = calculadora.Somar(10, 2);
+
+            //Assert
+           // Assert.True(resultado == 4);
+            Assert.Equal(4,resultado);
+
+        }
+    }
+}
