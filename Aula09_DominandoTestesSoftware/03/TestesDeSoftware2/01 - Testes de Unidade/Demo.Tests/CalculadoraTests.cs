cc6ffb4c1c6265d5eaa659ce1d45661a30e1a1bd
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-08-10 13:10:38 -0300

    update

--- Aula09_DominandoTestesSoftware/03/TestesDeSoftware2/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
+++ Aula09_DominandoTestesSoftware/03/TestesDeSoftware2/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -9,7 +9,7 @@ namespace Demo.Tests
     {
         /// Todos os métodos do teste são publicos 
 
-        [Fact]
+        [Fact] //Fato 2 +2 = 4 
         public void Calculadora_Somar_RetornarValorSoma()
         {
             //Arrange
@@ -18,7 +18,7 @@ namespace Demo.Tests
 
             //Act
 
-            var resultado = calculadora.Somar(9, 2);
+            var resultado = calculadora.Somar(2, 2);
 
             //Assert
            // Assert.True(resultado == 4);
@@ -26,8 +26,11 @@ namespace Demo.Tests
 
         }
 
-        [Theory]
+        [Theory] //Teoria não só uma soma é suficiente, provando e validando o método somar com demais valores e cenários para ser verdade
         [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        [InlineData(3,3,6)]
+        [InlineData(4,4,8)]
 
         public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1,double v2,double total)
         {
