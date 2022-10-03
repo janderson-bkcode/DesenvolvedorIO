704ca3e2f0cf1111bd5ec85d23904ea615ed5426
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-10-03 09:59:48 -0300

    Update AuthController.cs

--- Aula04_AspNetCoreEnterpriseApplications/NerdStore2/NerdStore-master (2)/NerdStore-master/src/services/NS.Identidade.API/Controllers/AuthController.cs
+++ Aula04_AspNetCoreEnterpriseApplications/NerdStore2/NerdStore-master (2)/NerdStore-master/src/services/NS.Identidade.API/Controllers/AuthController.cs
@@ -98,8 +98,8 @@ namespace NS.Identidade.API.Controllers
             claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
             claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
             claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));//quanto o token vai expirar
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));//Quando o token foi emitido
             foreach (var userRole in userRoles)
             {
                 claims.Add(new Claim("role", userRole));
