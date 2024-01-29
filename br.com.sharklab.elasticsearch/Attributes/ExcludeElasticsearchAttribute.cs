59d8f6296cd96363594d266f786bb28b3bc32ebb
Janderson Goncalves <janderson.goncalves@bkbank.com.br>
2024-01-29 15:01:08 -0300

    deleted Elastic

--- br.com.sharklab.elasticsearch/Attributes/ExcludeElasticsearchAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace br.com.sharklab.elasticsearch.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class)]
-    public class ExcludeElasticsearchAttribute : Attribute
-    {
-    }
-}
